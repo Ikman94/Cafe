5eb01f84694ae165c085726d53f8370d8930b54c
Ikechukwu Iwuh <59923488+Ikman94@users.noreply.github.com>
2021-06-22 16:28:25 +0100

    Publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -43,9 +43,10 @@ app.use('/api/', Routes);
 const _dirname = path.resolve();
 app.use('/uploads', express.static(path.join(_dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'frontend', 'build', 'index.html');
+    res.sendFile(index);
+});
 
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
